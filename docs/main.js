d19d24a88a8f7145b1ddfb543ee9079f2fb41638
tasogare3710 <tasogare.android@gmail.com>
2024-09-06 01:19:14 +0900

    モダン化とパラメタ化

--- docs/main.js
+++ docs/main.js
@@ -1,110 +1,159 @@
-var canvas, ctx , textbox, result;
+const FONT_FAMILY = "myfont";
 
-window.onload = function () {
-  canvas = document.createElement("canvas");
-  canvas.width = 1500;
-  canvas.height = 150;
-  textbox = document.getElementById("textbox");
-  result  = document.getElementById("result");
-  ctx = canvas.getContext('2d');
-  ctx.font = '100px Noto Sans JP';
-  ctx.lineJoin = 'round';
-};
+var canvas = document.getElementById("canvas");
 
-function draw() {
-  ctx.setTransform(1,0,0,1,0,0);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.setTransform(1,0,-0.4,1,0,0);
-  var posx = 70;
-  var posy = 100;
-  var text = textbox.value;
-     //黒色
-    {
-      ctx.strokeStyle = "#000";
-      ctx.lineWidth = 22;
-      ctx.strokeText(text, posx + 4, posy + 4);
-    }
+var canvasWidth = document.getElementById("canvasWidth");
+canvasWidth.addEventListener("input", resizeWidth);
 
-  //銀色
-  {
-    var grad = ctx.createLinearGradient(0, 24, 0, 122);
-    grad.addColorStop(0.0, 'rgb(0,15,36)');
-    grad.addColorStop(0.10, 'rgb(255,255,255)');
-    grad.addColorStop(0.18, 'rgb(55,58,59)');
-    grad.addColorStop(0.25, 'rgb(55,58,59)');
-    grad.addColorStop(0.5, 'rgb(200,200,200)');
-    grad.addColorStop(0.75, 'rgb(55,58,59)');
-    grad.addColorStop(0.85, 'rgb(25,20,31)');
-    grad.addColorStop(0.91, 'rgb(240,240,240)');
-    grad.addColorStop(0.95, 'rgb(166,175,194)');
+var canvasHeight = document.getElementById("canvasHeight");
+canvasHeight.addEventListener("input", resizeheight);
+
+var posX = document.getElementById("posX");
+posX.addEventListener("input", draw);
+
+var posY = document.getElementById("posY");
+posY.addEventListener("input", draw);
+
+var canvasZoom = document.getElementById("canvasZoom");
+canvasZoom.addEventListener("input", function(event){
+    canvas.style.zoom = `${event.target.value}%`;
+});
+
+var fontSize = document.getElementById("fontSize");
+fontSize.addEventListener("input", draw);
+
+var lineWidth = document.getElementById("lineWidth");
+lineWidth.addEventListener("input", draw);
+
+var textbox = document.getElementById("textbox");
+textbox.addEventListener("input", draw);
+
+var lineJoin = document.getElementById("lineJoin");
+lineJoin.addEventListener("input", draw);
+
+var gradient1 = document.getElementById("gradient1");
+gradient1.addEventListener("input", draw);
+
+gradient2 = document.getElementById("gradient2");
+gradient2.addEventListener("input", draw);
+
+gradient3 = document.getElementById("gradient3");
+gradient3.addEventListener("input", draw);
+
+gradient4 = document.getElementById("gradient4");
+gradient4.addEventListener("input", draw);
+
+var generateButton = document.getElementById("generateButton");
+generateButton.addEventListener("click", draw);
+
+var ctx = canvas.getContext('2d');
+
+function resizeWidth(event) {
+    canvas.setAttribute("width", canvasWidth.value);
+    draw();
+}
+
+function resizeheight(event) {
+    canvas.setAttribute("height", canvasHeight.value);
+    draw();
+}
+
+function draw(event) {
+	const NUMBER_INPUT_BASE = 1;
+	const COLOR_PICKER_BASE = NUMBER_INPUT_BASE + 5;
+    const posx = Number(posX.value);
+    const posy = Number(posY.value);
+    const LINE_WIDTH = Number(lineWidth.value);
+    const text = textbox.value;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.font = `${fontSize.value}px ${FONT_FAMILY}`;
+    ctx.lineJoin = lineJoin.value;
+
+    //黒色
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = LINE_WIDTH;
+    ctx.strokeText(text, posx + 4, posy + 4);
+
+    //銀色
+    var grad = ctx.createLinearGradient(
+        Number(gradient1.elements[NUMBER_INPUT_BASE + 0].value),
+        Number(gradient1.elements[NUMBER_INPUT_BASE + 1].value),
+        Number(gradient1.elements[NUMBER_INPUT_BASE + 2].value),
+        Number(gradient1.elements[NUMBER_INPUT_BASE + 3].value),
+    );
+    grad.addColorStop(0.0, gradient1.elements[COLOR_PICKER_BASE + 0].value);
+    grad.addColorStop(0.10, gradient1.elements[COLOR_PICKER_BASE + 1].value);
+    grad.addColorStop(0.18, gradient1.elements[COLOR_PICKER_BASE + 2].value);
+    grad.addColorStop(0.25, gradient1.elements[COLOR_PICKER_BASE + 3].value);
+    grad.addColorStop(0.5, gradient1.elements[COLOR_PICKER_BASE + 4].value);
+    grad.addColorStop(0.75, gradient1.elements[COLOR_PICKER_BASE + 5].value);
+    grad.addColorStop(0.85, gradient1.elements[COLOR_PICKER_BASE + 6].value);
+    grad.addColorStop(0.91, gradient1.elements[COLOR_PICKER_BASE + 7].value);
+    grad.addColorStop(0.95, gradient1.elements[COLOR_PICKER_BASE + 8].value);
     grad.addColorStop(1, 'rgb(50,50,50)');
     ctx.strokeStyle = grad;
-    ctx.lineWidth = 20;
+    ctx.lineWidth = LINE_WIDTH - 2;
     ctx.strokeText(text, posx + 4, posy + 4);
-  }
 
-  //黒色
-  {
+    //黒色
     ctx.strokeStyle = "#000000";
-    ctx.lineWidth = 16;
+    ctx.lineWidth = LINE_WIDTH - 6;
     ctx.strokeText(text, posx, posy);
-  }
 
-  //金色
-  {
-    var grad = ctx.createLinearGradient(0, 20, 0, 100);
-    grad.addColorStop(0, 'rgb(253,241,0)');
-    grad.addColorStop(0.25, 'rgb(245,253,187)');
-    grad.addColorStop(0.4, 'rgb(255,255,255)');
-    grad.addColorStop(0.75, 'rgb(253,219,9)');
-    grad.addColorStop(0.9, 'rgb(127,53,0)');
-    grad.addColorStop(1, 'rgb(243,196,11)');
+    //金色
+    var grad = ctx.createLinearGradient(
+        Number(gradient2.elements[NUMBER_INPUT_BASE + 0].value),
+        Number(gradient2.elements[NUMBER_INPUT_BASE + 1].value),
+        Number(gradient2.elements[NUMBER_INPUT_BASE + 2].value),
+        Number(gradient2.elements[NUMBER_INPUT_BASE + 3].value),
+    );
+    grad.addColorStop(0, gradient2.elements[COLOR_PICKER_BASE + 0].value);
+    grad.addColorStop(0.25, gradient2.elements[COLOR_PICKER_BASE + 1].value);
+    grad.addColorStop(0.4, gradient2.elements[COLOR_PICKER_BASE + 2].value);
+    grad.addColorStop(0.75, gradient2.elements[COLOR_PICKER_BASE + 3].value);
+    grad.addColorStop(0.9, gradient2.elements[COLOR_PICKER_BASE + 4].value);
+    grad.addColorStop(1, gradient2.elements[COLOR_PICKER_BASE + 5].value);
     ctx.strokeStyle = grad;
     ctx.lineWidth = 10;
     ctx.strokeText(text, posx, posy);
-  }
 
-  //黒
-  ctx.lineWidth = 6;
-  ctx.strokeStyle = '#000';
-  ctx.strokeText(text, posx+2, posy - 3);
+    //黒
+    ctx.lineWidth = 6;
+    ctx.strokeStyle = '#000';
+    ctx.strokeText(text, posx + 2, posy - 3);
 
-  //白
-  ctx.lineWidth = 6;
-  ctx.strokeStyle = '#FFFFFF';
-  ctx.strokeText(text, posx, posy - 3);
+    //白
+    ctx.lineWidth = 6;
+    ctx.strokeStyle = '#FFFFFF';
+    ctx.strokeText(text, posx, posy - 3);
 
-  //赤
-  {
-    var grad = ctx.createLinearGradient(0, 20, 0, 100);
-    grad.addColorStop(0, 'rgb(255, 100, 0)');
-    grad.addColorStop(0.5, 'rgb(123, 0, 0)');
-    grad.addColorStop(0.51, 'rgb(240, 0, 0)');
-    grad.addColorStop(1, 'rgb(5, 0, 0)');
+    //赤
+    var grad = ctx.createLinearGradient(
+        Number(gradient3.elements[NUMBER_INPUT_BASE + 0].value),
+        Number(gradient3.elements[NUMBER_INPUT_BASE + 1].value),
+        Number(gradient3.elements[NUMBER_INPUT_BASE + 2].value),
+        Number(gradient3.elements[NUMBER_INPUT_BASE + 3].value),
+    );
+    grad.addColorStop(0, gradient3.elements[COLOR_PICKER_BASE + 0].value);
+    grad.addColorStop(0.5, gradient3.elements[COLOR_PICKER_BASE + 1].value);
+    grad.addColorStop(0.51, gradient3.elements[COLOR_PICKER_BASE + 2].value);
+    grad.addColorStop(1, gradient3.elements[COLOR_PICKER_BASE + 3].value);
     ctx.lineWidth = 1;
     ctx.strokeStyle = grad;
     ctx.strokeText(text, posx, posy - 3);
-  }
-
-  //赤
-  {
-    var grad = ctx.createLinearGradient(0, 20, 0, 100);
-    grad.addColorStop(0, 'rgb(230, 0, 0)');
-    grad.addColorStop(0.5, 'rgb(123, 0, 0)');
-    grad.addColorStop(0.51, 'rgb(240, 0, 0)');
-    grad.addColorStop(1, 'rgb(5, 0, 0)');
+    //赤
+    var grad = ctx.createLinearGradient(
+        Number(gradient4.elements[NUMBER_INPUT_BASE + 0].value),
+        Number(gradient4.elements[NUMBER_INPUT_BASE + 1].value),
+        Number(gradient4.elements[NUMBER_INPUT_BASE + 2].value),
+        Number(gradient4.elements[NUMBER_INPUT_BASE + 3].value),
+    );
+    grad.addColorStop(0, gradient4.elements[COLOR_PICKER_BASE + 0].value);
+    grad.addColorStop(0.5, gradient4.elements[COLOR_PICKER_BASE + 1].value);
+    grad.addColorStop(0.51, gradient4.elements[COLOR_PICKER_BASE + 2].value);
+    grad.addColorStop(1, gradient4.elements[COLOR_PICKER_BASE + 3].value);
     ctx.fillStyle = grad;
     ctx.fillText(text, posx, posy - 3);
-  }
-}
-
-function loadFont() {
-  var text = textbox.value;
-  ctx.fillText(text, 0, 0);
-}
-
-function convertToImage(){
-  draw();
-  var data = canvas.toDataURL();
-  result.src = data;
 }
